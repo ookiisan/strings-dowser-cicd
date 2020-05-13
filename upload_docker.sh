835ef06488b7c7d39a42409e2d19eecc14f3fc28
Andrea Grande <ookiisan@gmail.com>
2020-05-13 07:24:37 +0200

    Added Docker Hub upload script

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `build_docker.sh`
+
+# Step 1:
+# Create dockerpath
+export imagename=strings-dowser
+export dockerpath=ookiisan/$imagename
+export version=1.0.0
+
+# Step 2:  
+# Authenticate & tag
+docker login
+docker tag $imagename:$version $dockerpath:$version
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:$version
