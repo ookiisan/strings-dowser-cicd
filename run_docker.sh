06f8810793240ab87cf0dfee3eadc73ba7901934
Andrea Grande <ookiisan@gmail.com>
2020-05-13 07:26:53 +0200

    Added Docker container run script (for local purpose)

---
+++ run_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Get Docker strings-dowser running locally
+
+# Step 1:
+# Build Docker image
+sh ./build_docker.sh
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app
+docker run --name strings-dowser -d -p 80:30001 strings-dowser:1.0.0
