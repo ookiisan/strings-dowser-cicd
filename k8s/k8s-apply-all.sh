9ad2b95307208e23c764928c4ec2d136675eb624
Andrea Grande <ookiisan@gmail.com>
2020-05-19 14:11:13 +0200

    Added version templating mechanism

--- k8s/k8s-apply-all.sh
+++ k8s/k8s-apply-all.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 # Applies deployment and load balancer service to k8s cluster
 
+export APP_VERSION=$(<../strings-dowser/version.txt)
+
+cp ./strings-dowser-deployment_tpl.yaml ./strings-dowser-deployment.yaml
+
+sed -i '' -e 's|<app-version-to-replace>|'"${APP_VERSION}"'|g' ./strings-dowser-deployment.yaml
+
 kubectl apply -f strings-dowser-deployment.yaml
+
 kubectl apply -f strings-dowser-service.yaml
