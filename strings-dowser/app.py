fdf472c73f6c8faf8cf64cf2406c70766ae26906
Andrea Grande <ookiisan@gmail.com>
2020-05-19 08:22:52 +0200

    Updated version 0.0.2

--- strings-dowser/app.py
+++ strings-dowser/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def ready():
-    return "Flask server is ready! And current version is 0.0.1"
+    return "Flask server is ready! And current version is 0.0.2"
 
 @app.route('/divine')
 def divine():
